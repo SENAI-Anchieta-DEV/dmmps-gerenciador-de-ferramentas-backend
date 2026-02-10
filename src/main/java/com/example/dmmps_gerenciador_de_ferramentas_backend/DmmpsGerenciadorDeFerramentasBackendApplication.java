a10b32b44b7eb3fa94db2046c1109913baf00ffa
Marcus Cruz <mvcruz0907@gmail.com>
2026-02-10 17:54:31 -0300

    chore: initial structure for Gerenciador de Ferramentas (Task 2.1)

---
+++ src/main/java/com/example/dmmps_gerenciador_de_ferramentas_backend/DmmpsGerenciadorDeFerramentasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.dmmps_gerenciador_de_ferramentas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmmpsGerenciadorDeFerramentasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmmpsGerenciadorDeFerramentasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dmmps_gerenciador_de_ferramentas_backend/DmmpsGerenciadorDeFerramentasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dmmps_gerenciador_de_ferramentas_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmmpsGerenciadorDeFerramentasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
