753f379bba33b2e0f1a1e6a60dd81cf58bc9fa86
Marcus Cruz <mvcruz0907@gmail.com>
2026-02-28 14:17:54 -0300

    DMMPS-107 feat: adições em "application.properties", "pom.xml", criação de AssinanteHandler e PublicadorService

---
+++ src/main/java/com/example/dmmps_gerenciador_de_ferramentas_backend/application/service/AssinanteHandler.java
@@ -0,0 +1,12 @@
+package com.example.dmmps_gerenciador_de_ferramentas_backend.application.service;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
+public class AssinanteHandler {
+//    @MqttSubscriber("topico/teste")
+//    public void receberMensagem(@MqttPayload String mensagem) {
+//        System.out.println("Mensagem recebida: " + mensagem);
+//    }
+}
---
+++ src/main/java/com/example/dmmps_gerenciador_de_ferramentas_backend/application/service/PublicadorService.java
@@ -0,0 +1,11 @@
+package com.example.dmmps_gerenciador_de_ferramentas_backend.application.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PublicadorService {
+//    @MqttPublisher("topico/teste")
+//    public String publicarMensagem() {
+//        return "Olá MQTT!";
+//    }
+}
