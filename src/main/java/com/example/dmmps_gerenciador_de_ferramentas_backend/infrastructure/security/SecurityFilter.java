90420845442348a9964b6919599c72003af0bb9f
Mateus Vieira Souza <mateusvs2420@gmail.com>
2026-03-05 10:46:17 -0300

    DMMPS-61 - Configuracao do SecurityFilterChain e PasswordEncoder

--- src/main/java/com/example/dmmps_gerenciador_de_ferramentas_backend/infrastructure/security/SecurityFilter.java
+++ src/main/java/com/example/dmmps_gerenciador_de_ferramentas_backend/infrastructure/security/SecurityFilter.java
@@ -1,11 +1,12 @@
 package com.example.dmmps_gerenciador_de_ferramentas_backend.infrastructure.security;
 
 import com.example.dmmps_gerenciador_de_ferramentas_backend.domain.repository.UsuarioRepository;
+import com.example.dmmps_gerenciador_de_ferramentas_backend.infrastructure.security.TokenService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,32 +18,34 @@ import java.io.IOException;
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
 
-    private final TokenService tokenService;
-    private final UsuarioRepository usuarioRepository;
+    @Autowired
+    private TokenService tokenService;
 
-    public SecurityFilter(TokenService tokenService, UsuarioRepository usuarioRepository) {
-        this.tokenService = tokenService;
-        this.usuarioRepository = usuarioRepository;
-    }
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
     @Override
-    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
-        var token = this.recoverToken(request);
-        if (token != null) {
-            var email = tokenService.validateToken(token);
-            UserDetails user = usuarioRepository.findByEmail(email);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJWT = recuperarToken(request);
 
-            if (user != null) {
-                var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-                SecurityContextHolder.getContext().setAuthentication(authentication);
+        if (tokenJWT != null) {
+            var subject = tokenService.getSubject(tokenJWT);
+            if (!subject.isEmpty()) {
+                UserDetails usuario = usuarioRepository.findByEmail(subject).orElse(null);
+                if (usuario != null) {
+                    var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
             }
         }
         filterChain.doFilter(request, response);
     }
 
-    private String recoverToken(HttpServletRequest request) {
-        var authHeader = request.getHeader("Authorization");
-        if (authHeader == null) return null;
-        return authHeader.replace("Bearer ", "");
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null) {
+            return authorizationHeader.replace("Bearer ", "");
+        }
+        return null;
     }
 }
